d888f20fe6ea0eba35bf703137ddaa38457b5e8e
Insta Web Works <59417571+instawebworks@users.noreply.github.com>
2024-09-17 18:14:47 +0600

    Update index.js

--- routes/xml/index.js
+++ routes/xml/index.js
@@ -1,7 +1,7 @@
 module.exports = async (fastify, opts) => {
   // define the about route
   fastify.get("/properties", async (request, reply) => {
-    const queryString = `SELECT * from properties limit 3000`;
+    const queryString = `SELECT * from properties limit 10`;
     const { rows: props, fields } = await fastify.epDbConn.query(queryString);
 
     let xml_str =
