20908c9a22f9beb8057e9c3f3b064a69f72b329c
Insta Web Works <59417571+instawebworks@users.noreply.github.com>
2024-09-30 18:37:17 +0600

    Update index.js

--- routes/xml/index.js
+++ routes/xml/index.js
@@ -88,17 +88,17 @@ module.exports = async (fastify, opts) => {
           xml_data,
         } = property;
         insert_properties.push(
-          `($${++count},$${++count},$${++count},$${++count}),$${++count})`
+          `($${++count},$${++count},$${++count},$${++count},$${++count})`
         );
         datas.push(crm_record_id);
         datas.push(status);
-        datas.push(product_id);
+        datas.push(product_id + "");
         datas.push(modified_time);
         datas.push(xml_data);
       }
       const queryString = `INSERT INTO properties (crm_record_id, status, product_id, modified_time, xml_data ) values ${insert_properties.join(
         ","
-      )} ON CONFLICT (crm_record_id) DO UPDATE SET xml_data = excluded.xml_data, status = excluded.status `;
+      )} ON CONFLICT (crm_record_id) DO UPDATE SET xml_data = excluded.xml_data, status = excluded.status, product_id = excluded.product_id `;
       const { rows: props, fields } = await fastify.epDbConn.query(
         queryString,
         datas
