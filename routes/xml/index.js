bb4b6609845ead8c5b4376c13b9fee18df31fd5d
tanveer-insta-web-works <tanveerctg@instawebworks.com.au>
2025-11-03 10:56:26 +0600

    Update index.js

--- routes/xml/index.js
+++ routes/xml/index.js
@@ -1449,7 +1449,54 @@ module.exports = async (fastify, opts) => {
             "- " + valueFromXML?._text || valueFromXML;
           return;
         }
-
+        //handle pool
+        if (key === "pool") {
+          const feature = [xmlJSON?.["features"]?.["feature"] || []].flat();
+          const isPrivatePoolFound = feature.find(
+            (itm) =>
+              itm?._text.toLowerCase().includes("pool") &&
+              itm?._text.toLowerCase().includes("private")
+          );
+          const isCommunalPoolFound = feature.find(
+            (itm) =>
+              itm?._text.toLowerCase().includes("pool") &&
+              itm?._text.toLowerCase().includes("communal")
+          );
+          const value =
+            serviceMap[isPrivatePoolFound?._text] ||
+            serviceMap[isCommunalPoolFound?._text] ||
+            "NO";
+          // console.log({
+          //   key,
+          //   crmApiKey,
+          //   valueFromXML,
+          //   feature,
+          //   isPrivatePoolFound,
+          //   isCommunalPoolFound,
+          //   value,
+          // });
+          updatedCRMJSON[crmApiKey] = value;
+          return;
+        }
+        // if (key === "pool" && (valueFromXML?._text || valueFromXML) != "1") {
+        //   const feature = [xmlJSON?.["features"]?.["feature"] || []].flat();
+        //   const isPrivatePoolFound = feature.find(
+        //     (itm) =>
+        //       itm?._text.toLowerCase().includes("pool") &&
+        //       itm?._text.toLowerCase().includes("private")
+        //   );
+        //   const isCommunalPoolFound = feature.find(
+        //     (itm) =>
+        //       itm?._text.toLowerCase().includes("pool") &&
+        //       itm?._text.toLowerCase().includes("communal")
+        //   );
+        //   console.log({
+        //     key,
+        //     valueFromXML: valueFromXML?._text || valueFromXML,
+        //     isPrivatePoolFound,
+        //     isCommunalPoolFound,
+        //   });
+        // }
         if (key === "feed_agent") {
           const ref = referenceKey;
           let prefix;
@@ -1554,10 +1601,10 @@ module.exports = async (fastify, opts) => {
               spaces: 2,
             }) +
             "</property>",
-          Properties: crmJSON?.[referenceKey]?.["id"],
-          Original_JSON: JSON.stringify(crmJSON?.[referenceKey]),
-          Update_Status: "Pending",
-          XML_Source: "homeespananewbuild",
+          // Properties: crmJSON?.[referenceKey]?.["id"],
+          // Original_JSON: JSON.stringify(crmJSON?.[referenceKey]),
+          // Update_Status: "Pending",
+          // XML_Source: "homeespananewbuild",
         });
         if (test != true) {
           if (updatedCRMData.length == 100) {
